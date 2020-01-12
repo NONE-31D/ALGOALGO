012d4d008472e91dafae478d6f21dda20c26ad1c
Yujin Lee <proqk@naver.com>
2020-01-12 22:36:18 +0900

    Create 1003 피보나치 함수.py

---
+++ proqk/1003 피보나치 함수.py
@@ -0,0 +1,27 @@
+dp0=[0]*100
+dp1=[0]*100
+
+dp0[0] = 1
+dp0[1] = 0
+dp1[0] = 0
+dp1[1] = 1
+
+
+T = int(input())
+
+while T>0 :
+
+    N = int(input())
+
+ 
+    for i in range(2, 41, 1) :
+
+        dp0[i] = dp0[i-2]+dp0[i-1]
+
+        dp1[i] = dp1[i-2]+dp1[i-1]
+
+    
+
+    print(dp0[N], dp1[N])
+
+    T-=1
